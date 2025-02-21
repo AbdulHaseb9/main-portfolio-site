f45cdd939c090f7eaa8bd893dd35938e6c961ac1
AbdulHaseb9 <haseebsheikh2380@gmail.com>
2025-02-22 02:06:59 +0500

    create sidebar file and style header on different screens

--- src/app/(components)/Header/Header.tsx
+++ src/app/(components)/Header/Header.tsx
@@ -3,16 +3,19 @@ import Link from "next/link";
 import { Container } from "@/components/Container";
 import { usePathname } from "next/navigation";
 import { routes, socialLinks } from "@/routes";
+import { HiOutlineBars3BottomRight } from "react-icons/hi2";
+import { useState } from "react";
 
 export const Header = () => {
   const pathname = usePathname();
+  const [sideNav, setSideNav] = useState(false);
 
   return (
     <header className="fixed top-0 left-0 right-0 z-10">
       <Container type="header" className="flex justify-between items-center">
         {/* Name & Navigation Links*/}
         <div className="flex items-center gap-x-28">
-          <h1 className="text-3xl font-semibold">
+          <h1 className="text-2xl font-semibold md:text-3xl">
             Haseeb <span className="text-yellow">Sheikh</span>
           </h1>
           {/* Links */}
@@ -31,17 +34,23 @@ export const Header = () => {
           </nav>
         </div>
         {/* Social Links */}
-        <div className="flex gap-x-2">
-          {socialLinks.map((social) => (
-            <Link
-              key={social.name}
-              href={social.path}
-              target="_blank"
-              title={social.name}
-            >
-              <social.icon className="text-2xl hover:text-white" />
-            </Link>
-          ))}
+        <div className="flex items-center gap-x-4 md:gap-x-7">
+          <div className="flex gap-x-2 md:gap-x-4">
+            {socialLinks.map((social) => (
+              <Link
+                key={social.name}
+                href={social.path}
+                target="_blank"
+                title={social.name}
+              >
+                <social.icon className="text-2xl hover:text-white" />
+              </Link>
+            ))}
+          </div>
+          <HiOutlineBars3BottomRight
+            onClick={() => setSideNav(!sideNav)}
+            className="text-2xl lg:hidden"
+          />
         </div>
       </Container>
     </header>
